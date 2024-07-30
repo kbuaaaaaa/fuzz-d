23daef045185ab19bb91897bb87a0b2dd3b25590
kbuaaaaaa <karnbongkot@gmail.com>
2024-07-30 18:03:18 +0100

    Change how java is executed

--- app/src/main/kotlin/fuzzd/validator/executor/execution_handler/JavaExecutionHandler.kt
+++ app/src/main/kotlin/fuzzd/validator/executor/execution_handler/JavaExecutionHandler.kt
@@ -4,5 +4,5 @@ class JavaExecutionHandler(fileDir: String, fileName: String) : AbstractExecutio
     override fun getCompileTarget(): String = "java"
 
     override fun getExecuteCommand(fileDir: String, fileName: String): String =
-        "java -cp $fileDir/$fileName-java:$fileDir/$fileName-java/DafnyRuntime.jar $fileDir/$fileName-java/$fileName.java"
+        "java -jar $fileDir/$fileName.jar"
 }
