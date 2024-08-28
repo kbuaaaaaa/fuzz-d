417494d9de8dde2bf25365d471f317eb0661ada6
kbuaaaaaa <karnbongkot@gmail.com>
2024-08-28 16:53:59 +0100

    Emit different python and java exec command for version 3.x

--- app/src/main/kotlin/fuzzd/validator/executor/execution_handler/JavaExecutionHandler.kt
+++ app/src/main/kotlin/fuzzd/validator/executor/execution_handler/JavaExecutionHandler.kt
@@ -3,6 +3,9 @@ package fuzzd.validator.executor.execution_handler
 class JavaExecutionHandler(fileDir: String, fileName: String, older: Int) : AbstractExecutionHandler(fileDir, fileName, older=older) {
     override fun getCompileTarget(): String = "java"
 
-    override fun getExecuteCommand(fileDir: String, fileName: String): String =
+    override fun getExecuteCommand(fileDir: String, fileName: String): String = if (older < 2) {
         "java -jar $fileDir/$fileName.jar"
+    } else {
+        "java -cp $fileDir/$fileName-java:$fileDir/$fileName-java/DafnyRuntime.jar $fileDir/$fileName-java/$fileName.java"
+    }
 }
--- app/src/main/kotlin/fuzzd/validator/executor/execution_handler/PyExecutionHandler.kt
+++ app/src/main/kotlin/fuzzd/validator/executor/execution_handler/PyExecutionHandler.kt
@@ -3,6 +3,9 @@ package fuzzd.validator.executor.execution_handler
 class PyExecutionHandler(fileDir: String, fileName: String, older: Int) : AbstractExecutionHandler(fileDir, fileName, older=older) {
     override fun getCompileTarget(): String = "py"
 
-    override fun getExecuteCommand(fileDir: String, fileName: String): String =
-        "python3 $fileDir/$fileName-py/__main__.py"
+    override fun getExecuteCommand(fileDir: String, fileName: String): String = if (older < 2) {
+            "python3 $fileDir/$fileName-py/__main__.py"
+        } else {
+            "python3 $fileDir/$fileName-py/$fileName.py"
+        }
 }
