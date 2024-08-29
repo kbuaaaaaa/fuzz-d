13c83db969bc2ad700e11c67dc55ab6488b2ad49
kbuaaaaaa <karnbongkot@gmail.com>
2024-08-29 16:08:34 +0100

    Change python exec command for version before 4.1.0

--- app/src/main/kotlin/fuzzd/validator/executor/execution_handler/PyExecutionHandler.kt
+++ app/src/main/kotlin/fuzzd/validator/executor/execution_handler/PyExecutionHandler.kt
@@ -5,6 +5,10 @@ import fuzzd.utils.compareVersions
 class PyExecutionHandler(fileDir: String, fileName: String, dafnyVersion: String) : AbstractExecutionHandler(fileDir, fileName, dafnyVersion=dafnyVersion) {
     override fun getCompileTarget(): String = "py"
 
-    override fun getExecuteCommand(fileDir: String, fileName: String): String = "python3 $fileDir/$fileName-py/__main__.py"
+    override fun getExecuteCommand(fileDir: String, fileName: String): String = if (compareVersions(dafnyVersion, "4.2.0") >= 0) {
+        "python3 $fileDir/$fileName-py/__main__.py"
+    } else {
+        "python3 $fileDir/$fileName-py/main.py"
+    }
 
 }
